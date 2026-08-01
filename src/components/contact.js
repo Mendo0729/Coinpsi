b88a18900c2c413e94a43c9b3a5c583874d469c7
Mendo0729 <Mendo0729@users.noreply.github.com>
2026-08-01 11:01:13 -0500

    Arreglo de la seccion de contacto

--- src/components/contact.js
+++ src/components/contact.js
@@ -31,7 +31,7 @@ export function renderContact(services) {
             ${contactInfoItem(
               "Mail",
               "Correo institucional",
-              '<a href="mailto:contacto@coinpsi.com">contacto@coinpsi.com</a>',
+              '<a href="mailto:contacto@coinpsi.org">contacto@coinpsi.org</a>',
               "Consultas corporativas y solicitudes de información"
             )}
           </div>
